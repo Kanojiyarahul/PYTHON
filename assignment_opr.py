43358297bb4ddb999aa2b7138e810560bae6ab39
Rahul Kanojiya <98940442+Kanojiyarahul@users.noreply.github.com>
2022-03-02 11:01:52 +0530

    Add files via upload

---
+++ assignment_opr.py
@@ -0,0 +1,34 @@
+# +=
+a=10
+b=20
+a+=b
+print(a)
+
+#-=
+a=10
+b=20
+a-=b
+print(a)
+
+# /=
+a=10
+b=20
+a/=b
+print(a)
+
+# %=
+a=20
+b=3
+a%=b
+print(a)
+
+# *=
+a=20
+b=3
+a*=b
+print(a)
+
+a=20
+b=3
+a^=5
+print(a)
