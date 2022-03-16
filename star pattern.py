02d1c4561b92cb996de6c84d887781b8dcb2ff0a
Rahul Kanojiya <98940442+Kanojiyarahul@users.noreply.github.com>
2022-03-16 09:04:16 +0530

    Add files via upload

---
+++ star pattern.py
@@ -0,0 +1,24 @@
+#1.star pattern
+for i in range(1,5):
+    for j in range(i):
+        print("*",end="")
+    print()
+
+#2.star pattern
+for i in range(4,0,-1):
+    for j in range(i):
+        print("*",end="")
+        
+    print()
+
+#3.star pattern
+for i in range(1,5):
+    for j in range(i):
+        print("*",end="")
+    print()
+for i in range(3,0,-1):
+    for j in range(i):
+        print("*",end="")
+        
+    print()
+
