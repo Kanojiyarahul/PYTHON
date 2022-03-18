c77658eee3ce3a9318bfb79913bfa7424c0d7e53
Rahul Kanojiya <98940442+Kanojiyarahul@users.noreply.github.com>
2022-03-18 10:39:21 +0530

    Add files via upload

---
+++ add_naturalno_for_loop.py
@@ -0,0 +1,8 @@
+#WAP to print the sum of  1st nth natural numbers using while loop
+
+n=int(input("enter number:"))
+add=0
+for i in range(1,n+1):
+    add=add+i
+print("addition of natural no:",add)
+    
