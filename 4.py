29b1ea2456ca3e2061103ce98d38ef54e853027e
Rahul Kanojiya <98940442+Kanojiyarahul@users.noreply.github.com>
2022-03-24 13:36:23 +0530

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,4 @@
+#4-temperature in degree celsius is entered by the user. WAP to convert temperature from degree celsius to degree fahrenheit.
+temp=int(input("enter temperature:"))
+print("degree celsius:",temp)
+print("degree fahrenheit:",(temp*1.8+32))
