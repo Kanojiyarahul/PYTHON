cff1ad8f2c47acb93351f4504779b399eb2df5f0
Rahul Kanojiya <98940442+Kanojiyarahul@users.noreply.github.com>
2022-03-24 13:36:46 +0530

    Add files via upload

---
+++ 5.py
@@ -0,0 +1,6 @@
+#5-A three four number is enter through the keyboard. WAP to find sum of the digit and also find the reverse of the four digit number.
+a,b,c,d=10,20,30,40
+print(a+b+c+d)
+num="9754"
+reverse=num[::-1]
+print(reverse)
