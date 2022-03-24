1f9f61383517b30997c4fce4cf73e29405bfae31
Rahul Kanojiya <98940442+Kanojiyarahul@users.noreply.github.com>
2022-03-24 13:35:49 +0530

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,4 +1,13 @@
-for i in range(1,6,1):
-    for j in range(1,6,1):
-        print("*",end=" ")
-    print()
+#1.1-write a python code to find area of circle
+ci=3.14
+rad=15
+area=("area of circle:",ci*rad*rad)
+print(area)
+
+#1.2-write a python code to accept 2 numbers from user and display add,sub,mul and div
+num1=34.5
+num2=7
+print("addition of number is:",num1+num2)
+print("subtraction of number is:",num1-num2)
+print("multiplication of number is:",num1*num2)
+print("division of number is:",num1/num2)
