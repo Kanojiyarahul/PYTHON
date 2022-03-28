e05b0b2016162c05ff6d9748cf1b9bda22caf89b
Rahul Kanojiya <98940442+Kanojiyarahul@users.noreply.github.com>
2022-03-28 12:14:56 +0530

    Add files via upload

---
+++ pattern.practice.py
@@ -0,0 +1,36 @@
+#1
+for i in range(1,6):
+    for j in range(1,6):
+        print("*",end=" ")
+    print()
+
+#2
+for i in range(1,6,1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
+
+#3
+for i in range(5,0,-1):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+#4
+for i in range(1,6):
+    for j in range(i):
+        print(j+1,end=" ") 
+    print()
+
+
+#5
+for i in range(1,6,1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()
+for i in range(4,0,-1):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+#6
