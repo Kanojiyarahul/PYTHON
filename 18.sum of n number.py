34ef3ff6d6ddc4ce72a59f1e9fa70cbcc22c9cbd
Rahul Kanojiya <98940442+Kanojiyarahul@users.noreply.github.com>
2022-04-01 09:37:41 +0530

    Add files via upload

---
+++ 18.sum of n number.py
@@ -0,0 +1,8 @@
+#wap to enter no till the user wants & at the end it should display sum of all no entered
+n=int(input("enter any number:"))
+sum=0
+i=0
+while(i<=n):
+    sum=sum+i
+    i+=1
+print("sum is:",sum)
