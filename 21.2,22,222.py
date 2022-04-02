6402921f415d520b97a8153c3eeff3d3842929fa
Rahul Kanojiya <98940442+Kanojiyarahul@users.noreply.github.com>
2022-04-02 10:35:20 +0530

    Add files via upload

---
+++ 21.2,22,222.py
@@ -0,0 +1,9 @@
+#Write a program to print the following series till n terms.
+#2 , 22 , 222 , 2222 _ _ _ _ _ n terms
+n=int(input("enter no :"))
+st='2'
+i=0
+while(i<n):
+    print(st,end=" , ")
+    st=st+'2'
+    i=i+1
