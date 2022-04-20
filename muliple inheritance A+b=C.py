d0b1f6d2ca910cd7ee60e7da27460c990cd7de6d
Rahul Kanojiya <98940442+Kanojiyarahul@users.noreply.github.com>
2022-04-20 12:34:14 +0530

    Add files via upload

---
+++ muliple inheritance A+b=C.py
@@ -0,0 +1,65 @@
+def area_cube( ):
+     side = int(input("Enter side of cube"))
+     print("Volume of cube is " , side*side*side)
+
+
+
+
+
+
+
+
+
+
+
+
+##class a:
+##    def __init__(self,length):
+##        self.length=length
+##    def sa(self):
+##        print(self.length)
+##
+##class b:
+##    def __init__(self,breadth):
+##        self.breadth=breadth
+##    def sb(self):
+##        print(self.breadthh)
+##        
+##class c(a,b):
+##    def __init__(self,length,breadth):
+##        super().__init__(length)
+##        super().__init__(breadth)
+##
+##    def area(self,area):
+##        self.area=self.length*self.breadth
+##        print("area",self.area)
+##o=c(4,5)
+
+
+
+##class emp():
+##    def __init__(self,name,age,salary):
+##        
+##        self.age=age
+##        self.name=name
+##        self.salary=salary
+##        self.email=f"{self.name.lower()}itvedant@gmail.com"
+##
+##    def intro(self):
+##        print(self.age)
+##        print(self.name)
+##        print(self.salary)
+##        print(self.email)
+##        
+##                 
+##
+##class developer(emp):
+##    def __init__(self,name,age,salary,prolong):
+##        
+##        super().__init__(name,age,salary)
+##        self.prolong=prolong
+##
+##    def intro(self):
+##        
+##        super().intro()
+##        print(self.prolong)
