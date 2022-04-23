be993bae895f71480bcade1076091b178f2d80fb
Rahul Kanojiya <98940442+Kanojiyarahul@users.noreply.github.com>
2022-04-23 15:48:22 +0530

    Add files via upload

---
+++ MAP.py
@@ -0,0 +1,27 @@
+###MAP-cube
+##numbers=[2,3,4,5,6]
+##def cube(number):
+##    return number**3
+##cube_of_numbers=list(map(cube,numbers))
+##print(cube_of_numbers)
+
+
+###MAP-listing
+##color=['red','black','orange','purple']
+##result=list(map(list,color))
+##print(result)
+
+###MAP-pow
+##bases_num = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+##index = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+##result=list(map(pow,bases_num,index))
+##print(result)
+##
+###MAP
+##charct = {'a', 'b', 'E', 'f', 'a', 'i', 'o', 'U', 'a'}
+##def change_cases(s):
+##    return str(s).upper(), str(s).lower()
+##result=map(change_cases,charct)
+##print(set(result))
+
+    
